3cc913178bfc902e1ae3c888b2d66c178ddb878f
tai <onwebbe@hotmail.com>
2019-01-15 23:17:32 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="dashBoardHeader">
+      Header Title
+    </div>
+    <div class="dashBoardContainer">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+  components: {
+  },
+  created () {
+    // let windowHeight = window.outerHeight;
+    // document.getElementById('app').style.height = windowHeight + 'px';
+  },
+  mounted () {
+    // let windowHeight = window.outerHeight;
+    // document.getElementById('app').style.height = windowHeight + 'px';
+  }
+}
+</script>
+<style lang="scss" type="text/css">
+@import './themeing/black';
+body {
+  border: 0px !important;
+  margin: 0px !important;
+  background-color: $backgroundColor-normal;
+  overflow: hidden;
+  height: 100vh;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  > .dashBoardHeader {
+    height: 50px;
+  }
+
+  > .dashBoardContainer {
+    flex: 1;
+  }
+}
+</style>
---
+++ src/views/DashBoard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboardMain">
+    <div class="dashBoardInner">
+      <div class="dashBoardTile">
+        <div>
+          title
+        </div>
+        <div>
+          content
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DashBoard',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" type="text/css">
+@import '../themeing/black';
+@import '../../node_modules/bulma';
+.dashboardMain {
+  background-color: $backgroundColor-normal;
+  width: calc( 100% - 20px );
+  height: calc( 100% - 20px );
+  color: $fontColor-normal;
+  padding: 10px;
+
+  > div.dashBoardInner {
+    border: 1px solid $borderColor-normal;
+    width: calc( 100% - 2px );
+    height: calc( 100% - 2px );
+  }
+
+  .dashBoardTile {
+    background-color: $tileBackgroundColor-normal;
+    height: 100px;
+    width: 100px;
+    margin: $tileMargin-normal;
+    padding: 5px;
+  }
+}
+</style>
