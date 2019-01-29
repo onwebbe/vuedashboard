6ece97100b7b1664aa66ed62e8583e10b7e4161f
tai <onwebbe@hotmail.com>
2019-01-29 21:33:08 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,15 @@
 const webpack = require('webpack')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuedashboard/'
+    : '/',
   devServer: {
     host: '0.0.0.0',
     port: 9000,
     https: false, // https:{type:Boolean}
     open: true,
-    proxy: 'http://localhost:5500'
+    proxy: 'http://localhost:3000'
   },
   configureWebpack: {
     plugins: [
